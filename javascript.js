2cbd8e4da88e073f1b3f51bfc7e29209564137fc
anisalatyan <anisalatyan@gmail.com>
2017-09-12 19:55:34 +0400

    initial commit

---
+++ javascript.js
@@ -0,0 +1,20 @@
+const num1=17;
+const num2=23;
+console.log (num1+num2);
+
+const text1='Java';
+const text2='Script';
+console.log (text1+text2)
+
+const num4=7;
+const num5=3;
+console.log (num4*num5) ;
+
+const num3=15;
+
+if(num3<18) {
+	console.log ("You need parents permission")
+}
+if(num3>=18){
+	console.log ("Give us your contacts")
+}
